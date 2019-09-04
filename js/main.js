aaf3fceebf1f1e5e82099c1dc0b52b51a5033c7e
ranariad <48047510+ranariad@users.noreply.github.com>
2019-09-05 00:11:28 +0200

    new files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/** var nav = document.getElementById('nav');
+
+window.onscroll = function () {
+    
+    
+    
+    if (window.pageXOffset > 100 ){
+        nav.style.position = "fixed";
+        nav.style.top = 0 ;
+    } else {
+        box.style.position = "absolute";
+        box.style.top = 100;
+    }
+}
+**/
